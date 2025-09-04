2f4b115cdb61d3247d0dfbe2f01475b434fa4338
networkedaudio <58989881+networkedaudio@users.noreply.github.com>
2025-09-03 20:25:26 -0500

    Making it work again

--- sources/DanteWrapperApp/Program.cs
+++ sources/DanteWrapperApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using DanteWrapperLibrary;
 
 namespace DanteWrapperApp
@@ -7,7 +8,18 @@ namespace DanteWrapperApp
     {
         private static void Main()
         {
-            foreach (var info in DanteRoutingApi.GetRxChannels("DESKTOP-VSC"))
+
+  
+
+            GetDeviceListAsync();
+
+            /*
+            using var device = new DanteBrowsing();
+            foreach (var name in await DanteBrowsing.GetDeviceNamesAsync())
+
+                DanteWrapperLibrary.RoutingDevice deviceInfo = new DanteWrapperLibrary.RoutingDevice(deviceNames[0]);
+
+            foreach (var info in deviceInfo.GetRxChannels())
             {
                 Console.WriteLine($"Id: {info.Id}");
                 Console.WriteLine($"IsStale: {info.IsStale}");
@@ -21,6 +33,40 @@ namespace DanteWrapperApp
                 Console.WriteLine($"Flow: {info.Flow}");
                 Console.WriteLine();
             }
+
+            */
+            Console.ReadLine();
+        }
+        private static async Task GetDeviceListAsync()
+        {
+
+            var foundNames = await DanteBrowsing.GetDeviceNamesAsync(new TimeSpan(0, 0, 1));
+
+            foreach (var name in foundNames)
+            {
+                Console.WriteLine(name);
+
+                RoutingDevice deviceInfo = new RoutingDevice(name);
+
+
+                var labels = deviceInfo.GetTxLabels();
+
+                foreach (var info in deviceInfo.GetRxChannels())
+                {
+                    Console.WriteLine($"Id: {info.Id}");
+                    Console.WriteLine($"IsStale: {info.IsStale}");
+                    Console.WriteLine($"Name: {info.Name}");
+                    Console.WriteLine($"Format: {info.Format}");
+                    Console.WriteLine($"Latency: {info.Latency}");
+                    Console.WriteLine($"IsMuted: {info.IsMuted}");
+                    Console.WriteLine($"SignalReferenceLevel: {info.SignalReferenceLevel}");
+                    Console.WriteLine($"Subscription: {info.Subscription}");
+                    Console.WriteLine($"Status: {info.Status}");
+                    Console.WriteLine($"Flow: {info.Flow}");
+                    Console.WriteLine();
+                }
+            }
+                
         }
     }
 }
